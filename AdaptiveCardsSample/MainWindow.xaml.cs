55cbeaff72e655645d295178280a793d7dde52f1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-03-11 11:23:53 +0100

    Add Adaptive Cards

--- AdaptiveCardsSample/MainWindow.xaml.cs
+++ AdaptiveCardsSample/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AdaptiveCards;
+using AdaptiveCards.Rendering;
+using AdaptiveCards.Rendering.Wpf;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdaptiveCardsSample
 {
@@ -20,9 +14,74 @@ namespace AdaptiveCardsSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly AdaptiveHostConfig _hostconfig;
+
         public MainWindow()
         {
             InitializeComponent();
+            webBrowser.SuppressScriptErrors(true);
+            _hostconfig = new AdaptiveHostConfig
+            {
+                Actions =
+                {
+                    ShowCard =
+                    {
+                        ActionMode = ShowCardActionMode.Popup
+                    }
+                }
+            };
+        }
+
+        private void OnShowCard(object sender, RoutedEventArgs e)
+        {
+            void showSubmitAction(RenderedAdaptiveCard card, AdaptiveSubmitAction action)
+            { }
+
+            var renderer = new AdaptiveCardRenderer(_hostconfig);
+            var version = renderer.SupportedSchemaVersion;
+            // renderer.UseXceedElementRenderers();
+
+            var json = LoadJson();
+            var result = AdaptiveCard.FromJson(json);
+            var renderedCard = renderer.RenderCard(result.Card);
+            renderedCard.OnAction += (card, args) =>
+            {
+
+            };
+
+            grid1.Children.Clear();
+            grid1.Children.Add(renderedCard.FrameworkElement);
+        }
+
+        private string LoadJson()
+        {
+            var picker = new OpenFileDialog
+            {
+                DefaultExt = ".json",
+                Filter = "JSON Files (.json)|*.json"
+            };
+
+            if (picker.ShowDialog() == true)
+            {
+                string json = File.ReadAllText(picker.FileName);
+                return json;
+            }
+            return string.Empty;
+        }
+    }
+
+    public static class WebBrowserExtensions
+    {
+        public static void SuppressScriptErrors(this WebBrowser webBrowser, bool hide)
+        {
+            FieldInfo fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null)
+                return;
+            object objComWebBrowser = fiComWebBrowser.GetValue(webBrowser);
+            if (objComWebBrowser == null)
+                return;
+
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
         }
     }
 }
